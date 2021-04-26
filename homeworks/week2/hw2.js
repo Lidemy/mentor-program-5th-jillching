fbbd706860db3ee60a41c2d1c2a2f494f05bf99f
jillching <rain20021213@gmail.com>
2021-04-26 19:47:02 +0800

    week02

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,7 @@
 function capitalize(str) {
-  
-}
+    var res = ''
+    res = str[0].toUpperCase() + str.slice(1)
+    return res
 
+}
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,17 @@
 function join(arr, concatStr) {
-  
+    var resJoin = ''
+    for (i = 0; i < arr.length; i++) {
+        resJoin += arr[i] + concatStr
+    }
+    return resJoin
 }
 
 function repeat(str, times) {
-  
+    var resRepeat = ''
+    for (i = 1; i <= times; i++) {
+        resRepeat += str
+    }
+    return resRepeat
 }
 
 console.log(join(['a'], '!'));
